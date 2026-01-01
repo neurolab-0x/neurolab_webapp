093aa3b55ad4e9e9b1fd7dede262dc7554a12021
Neurolab Dev <dev@neurolab.local>
2026-01-01 12:39:19 +0200

    chore: misc updates

--- src/api/dashboardData.ts
+++ src/api/dashboardData.ts
@@ -3,18 +3,18 @@ import axios from "axios";
 // Fetches live analysis data for a user by _id
 export async function getDashboardCardsData() {
     try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/analysis/user`);
+        const response = await axios.get('analysis/user');
         // Optionally, return the latest analysis
         const analyses = response.data;
         return analyses;
     } catch (error: any) {
         return error?.response?.data?.message || "Failed to fetch analysis data.";
     }
-}
+} 
 
 export async function getTotalActivities() {
     try {
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/analysis/user`);
+        const response = await axios.get('analysis/user');
         // Optionally, return the latest analysis
         const analyses = response.data;
         return analyses;
